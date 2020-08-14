ac052d6ed348fd230bb988f1c3bb7bfcf965eaa2
Ferhat Yılmaz <dore.fy@gmail.com>
2020-08-14 08:31:48 +0300

    Function "generateRandPool()": Added.
    By this function random number pool gen be generated for the random
    number structures. Random number pool size is hard coded as (1x9999)
    vector.
    
     Changes to be committed:
    	new file:   lib/generateRandPool.m

---
+++ lib/generateRandPool.m
@@ -0,0 +1,43 @@
+ %% بسم الله الرحمن الرحیم 
+
+%% ## Random Number Pool ##
+% 10.08.2020, Ferhat Yılmaz
+
+%% Description
+%{
+	Function to generate vectorial pool filled by random
+	numbers with given method. Vector size designated as
+	9999 because of "truerand()" limit.
+
+>> Inputs:
+	1. <randStructure> : structure :  random number structure
+
+<< Outputs:
+	1. <randStructure> : structure : random number structure
+%}
+
+%%
+function randStructure = generateRandPool(randStructure)
+	% Add path of TRNG function
+	addpath('./trurand/');
+	
+	% Size is designated as 9999 because of "truerand()" limits
+	size = 9999;
+	
+	% Check randomization method from structure
+	randMethod = randStructure.method;
+	
+	switch randMethod
+		% If method is TRNG
+		case 'TRNG'
+			randStructure.pool = truerand(1, size, randStructure.lowerNumber, randStructure.upperNumber);
+			randStructure.index = size;
+		% If method is PRNG
+		case 'PRNG'
+			randStructure.pool = randi([randStructure.lowerNumber, randStructure.upperNumber], 1, size);
+			randStructure.index = size;
+		% Otherwise
+		otherwise
+			msg = 'Randomization method  is undefined.';
+			error(msg);
+end
