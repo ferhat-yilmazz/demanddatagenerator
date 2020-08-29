0c7ba46afee0328e73cc3b4d37d3f7caaf253b67
Ferhat Yılmaz <dore.fy@gmail.com>
2020-08-29 15:16:57 +0300

    Global variables edited.
     Changes to be committed:
    	modified:   globalVariables.m

--- lib/globalVariables.m
+++ lib/globalVariables.m
@@ -15,18 +15,21 @@ global COUNT_END_USERS
 % Count of weeks
 global COUNT_WEEKS
 % Try limit
-global TRY_LIMIT
+global DAY_PIECE
 % Sample period
 global SAMPLE_PERIOD
 % Count of sample in an hour
 global COUNT_SAMPLE_IN_HOUR
 % Count of sample in a day
 global COUNT_SAMPLE_IN_DAY
+% Sample2Time vector
+global TIME_VECTOR
 
 %% Definition of global variables
 COUNT_END_USERS = initialConditions.endUserCount;
 COUNT_WEEKS = initialConditions.weekCount;
-TRY_LIMIT = initialConditions.tryLimit;
+DAY_PIECE = initialConditions.dayPiece;
 SAMPLE_PERIOD = initialConditions.samplePeriod;
-COUNT_SAMPLE_IN_HOUR = ceil(60/SAMPLE_PERIOD);
+COUNT_SAMPLE_IN_HOUR = floor(60/SAMPLE_PERIOD);
 COUNT_SAMPLE_IN_DAY = 24*COUNT_SAMPLE_IN_DAY;
+TIME_VECTOR = generateTimeVector;
