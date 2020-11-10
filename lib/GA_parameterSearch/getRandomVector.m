cd3b2878a135dd9b4c3d012cf8a046fda150d931
Ferhat Yılmaz <dore.fy@gmail.com>
2020-11-10 14:28:42 +0300

    "getRandomVector()": Added.
    Function to generate row vector contains random numbers as count as
    requested. Randomization method is parametric: TRNG or PRNG. The
    function is similar to random structure functions in the "dataGenerator"
    section. However, "genetic algorithm parameter search" group will have
    different function as possible as.
    
     Changes to be committed:
    	new file:   GA_parameterSearch/getRandomVector.m
    
    
    Former-commit-id: 5473d983da3d86290b33d0fec75052b5bacd3490

---
+++ lib/GA_parameterSearch/getRandomVector.m
@@ -0,0 +1,40 @@
+ %% بسم الله الرحمن الرحیم 
+
+%% ## Get Random Number List ##
+% 09.10.2020, Ferhat Yılmaz
+
+%% Description
+%{
+	Function to get random number list as count as requested.
+	Randomization method is a parameter: 'TRNG' or 'PRNG'.
+
+	>> Inputs:
+	1. <vectorSize> : integer : Count of requested random number
+	2. <minLimit> : integer : Minimum limit of random numbers
+	3. <maxLimit> : integer : Maximum limit of random numbers
+	4. <randMethod> : string :  Randomization method.
+
+<< Outputs:
+	1. <randVector> : vector : Vectors of generated random numbers
+%}
+
+%%
+function randVector = getRandomVector(vectorSize, minLimit, maxLimit, randMethod)
+	% Add path of TRNG function
+	addpath('../trueRandom/');
+	
+	switch randMethod
+		case 'TRNG'
+			try
+				randVector = truerand(1, vectorSize, minLimit, maxLimit);
+			catch ME
+				warning(ME.message);
+				warning('PRNG will be used as randomization method!');
+				randVector = randi([minLimit, maxLimit], 1, vectorSize);
+			end
+		case 'PRNG'
+			randVector = randi([minLimit, maxLimit], 1, vectorSize);
+		otherwise
+				error('Undefined randomization method is given.');
+	end
+end
