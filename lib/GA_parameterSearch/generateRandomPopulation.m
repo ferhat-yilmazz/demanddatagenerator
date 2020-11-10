ba223d549db411a7e42c114d001f3d3b39861ce3
Ferhat Yılmaz <dore.fy@gmail.com>
2020-11-10 17:03:40 +0300

    "generateRandomPopulation()": Added.
    Genetic algorithm: random population generation module added.
    
     Changes to be committed:
    	new file:   generateRandomPopulation.m
    
    
    Former-commit-id: 256bd01ee9d6c0dfd7d9b3a48ff5a878e8d50abc

---
+++ lib/GA_parameterSearch/generateRandomPopulation.m
@@ -0,0 +1,29 @@
+ %% بسم الله الرحمن الرحیم 
+
+%% ## Generate Random Population ##
+% 09.10.2020, Ferhat Yılmaz
+
+%% Description
+%{
+	Function to generate solution population randomly. Randomization
+	method will accepted as parameter.
+
+	>> Inputs:
+	1. <chromosomeCount> : integer : Count of requested random number
+	2. <geneCount> : integer : Minimum limit of random numbers
+	3. <geneLowLimit> : integer : Lower limit of each coefficient
+	4. <geneUpLimit> : integer : Upper limit of each coefficient
+	5. <randMethod> : string :  Randomization method.
+
+<< Outputs:
+	1. <populationArray> : array : Array contains chromosomes
+%}
+
+%%
+function populationArray = generateRandomPopulation(chromosomeCount, geneCount, geneLowLimit, geneUpLimit, randMethod)
+	% Get all genes randomly
+	randomGenes = getRandomVector(geneCount*chromosomeCount, geneLowLimit, geneUpLimit, randMethod);
+	
+	% Assign acquired genes to chromosomes
+	populationArray = transpose(reshape(randomGenes, [geneCount, chromosomeCount]));
+end
