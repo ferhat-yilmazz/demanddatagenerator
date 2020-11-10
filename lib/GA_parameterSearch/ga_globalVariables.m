f26c8f51319248168c92e2d4e561e563682f66e9
Ferhat Yılmaz <dore.fy@gmail.com>
2020-11-10 17:01:49 +0300

    "ga_globalVariables.m": Added.
    Script to define global variables for genetic algorithm parameter search
    added.
    
     Changes to be committed:
    	new file:   ga_globalVariables.m
    
    
    Former-commit-id: 8a7aae54b6b8269e4d7da03b098fd6e474d504b2

---
+++ lib/GA_parameterSearch/ga_globalVariables.m
@@ -0,0 +1,38 @@
+ %% بسم الله الرحمن الرحیم 
+
+%% ## Genetic Algorithm Global Variables ##
+% 10.11.2020, Ferhat Yılmaz
+
+%% Description
+%{
+	Script to define global variables. These variables are used
+	in all program and they are never changed (READ ONLY) in runtime.
+%}
+
+%% Declare global variables
+% Count of weeks
+global COUNT_WEEKS;
+% Try limit
+global DAY_PIECE;
+% Sample period
+global SAMPLE_PERIOD;
+% Count of sample in a day
+global COUNT_SAMPLE_IN_DAY;
+% Sample2Time vector
+global TIME_VECTOR;
+% Rand method
+global RAND_METHOD;
+% Global maximum operation duration
+global GLOB_MAX_OPERATION_LIMIT;
+
+%% Definition of global variables
+COUNT_WEEKS = initialConditions.weekCount;
+DAY_PIECE = initialConditions.dayPiece;
+SAMPLE_PERIOD = minutes(double2duration(initialConditions.samplePeriod, '24h'));
+GLOB_MAX_OPERATION_LIMIT = duration2sample(double2duration(initialConditions.globalMaxOperationLimit, 'inf'));
+% Check for sample period sub-multiple of minutes in a day.
+msg = 'Please edit sample period as sub-multiple of minutes in a day!';
+assert(mod(24*60, SAMPLE_PERIOD) == 0, msg);
+COUNT_SAMPLE_IN_DAY = (24*60)/SAMPLE_PERIOD;
+TIME_VECTOR = generateTimeVector;
+RAND_METHOD = 'PRNG';
