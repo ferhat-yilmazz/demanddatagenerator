a8dbfe5b38ee41faf14c3f5b1228c649c956a097
Ferhat Yılmaz <dore.fy@gmail.com>
2020-11-10 17:00:40 +0300

    "ga_loadConfigurations.m": Added.
    Script to load configuration files for genetic algorithm parameter
    search added.
    
     Changes to be committed:
    	new file:   ga_loadConfigurations.m
    
    
    Former-commit-id: 677d80fb1aae8ab2e23de8fea767881fab4a4fdf

---
+++ lib/GA_parameterSearch/ga_loadConfigurations.m
@@ -0,0 +1,23 @@
+ %% بسم الله الرحمن الرحیم 
+
+%% ## Genetic Algorithm Load Configuration Files ##
+% 09.11.2020, Ferhat Yılmaz
+
+%% Description
+%{
+	Script to load configuration files. Paths are also defined in
+	this script.
+%}
+
+%% Define PATH variables
+% "appliancesData.json" path
+PATH_applianceData = '../configs/appliancesData.json';
+% "residentalTypes.json" path
+PATH_residentalTypes = '../configs/residentalTypes.json';
+% "initialCOnditions.json" path
+PATH_initialConditions = '../configs/initialConditions.json';
+
+%% Load config files as structure
+appliancesData = loadJSONFile(PATH_applianceData);
+residentalTypes = loadJSONFile(PATH_residentalTypes);
+initialConditions = loadJSONFile(PATH_initialConditions);
