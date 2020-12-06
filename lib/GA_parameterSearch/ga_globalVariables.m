30b3c513b146437dcc542fd258ec4991810284e8
Ferhat Yılmaz <dore.fy@gmail.com>
2020-12-06 11:28:38 +0300

    ga_globalVariables.m: Random setting changed.
    Randomization method will be acquired from configuration file anymore.
    
     Changes to be committed:
    	modified:   ga_globalVariables.m
    
    
    Former-commit-id: d562e56c206d880620742525eb506296d40400b7

--- lib/GA_parameterSearch/ga_globalVariables.m
+++ lib/GA_parameterSearch/ga_globalVariables.m
@@ -35,4 +35,6 @@ msg = 'Please edit sample period as sub-multiple of minutes in a day!';
 assert(mod(24*60, SAMPLE_PERIOD) == 0, msg);
 COUNT_SAMPLE_IN_DAY = (24*60)/SAMPLE_PERIOD;
 TIME_VECTOR = generateTimeVector;
-RAND_METHOD = 'PRNG';
+RAND_METHOD = initialConditions.ga_randomizationMethod;
+% Check randomization method is valid
+assert(strcmp(RAND_METHOD, 'TRNG') || strcmp(RAND_METHOD, 'PRNG'), "intialConditions.json:Randomization method is not valid!");
