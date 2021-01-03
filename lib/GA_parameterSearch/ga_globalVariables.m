83638f09984f11f027eb7fa2f04fd6c990bb5825
Ferhat Yılmaz <dore.fy@gmail.com>
2021-01-03 21:57:12 +0300

    "ga_globalVariables.m": Edited.
    New global variables added.
    
     Changes to be committed:
    	modified:   ga_globalVariables.m
    
    
    Former-commit-id: 65c2a458e777e24f073fc5736cacf5d4da3029fb

--- lib/GA_parameterSearch/ga_globalVariables.m
+++ lib/GA_parameterSearch/ga_globalVariables.m
@@ -24,6 +24,8 @@ global TIME_VECTOR;
 global RAND_METHOD;
 % Global maximum operation duration
 global GLOB_MAX_OPERATION_LIMIT;
+% Count of crossover methods
+global CROSSOVER_METHODS;
 % Count of chromosomes(count of individuals in a population)
 global COUNT_CHROMOSOMES;
 % Count of genes in each chromosome (count of parameters)
@@ -42,6 +44,16 @@ global COUNT_CHOSENS;
 global COUNT_ELITES
 % Count of offsprings
 global COUNT_OFFSPRINGS;
+% Initial mutation possibility of chromosomes (%)
+global MUTATION_CHROMOSOME_PERCENT;
+% Mutant chromosome effect to possibility
+global MUTANT_CHROMOSOME_EFFECT;
+% Initial mutation possibility of genes (%)
+global MUTATION_GENE_PERCENT;
+% Mutant gene effect to possibility
+global MUTANT_GENE_EFFECT;
+% Rate of mutation (%)
+global MUTATION_RATE;
 
 %% Definition of global variables
 COUNT_WEEKS = uint16(geneticAlgorithm.weekCount);
@@ -58,15 +70,25 @@ TIME_VECTOR = generateTimeVector;
 RAND_METHOD = geneticAlgorithm.randomizationMethod;
 % Check randomization method is valid
 assert(strcmp(RAND_METHOD, 'TRNG') || strcmp(RAND_METHOD, 'PRNG'), "intialConditions.json:Randomization method is not valid!");
-COUNT_CHROMOSOMES = geneticAlgorithm.algorithm.chromosomeCount;
-COUNT_GENES = uint16(geneticAlgorithm.algorithm.geneCount);
-GENE_LOW_LIMIT = uint16(geneticAlgorithm.algorithm.geneLowerLimit);
-GENE_UP_LIMIT = uint16(geneticAlgorithm.algorithm.geneUpperLimit);
-TERMINATION_ERROR_PERCENTAGE = single(geneticAlgorithm.algorithm.terminationPercentage);
-TERMINATION_CHROMOSOME_COUNT = uint16(geneticAlgorithm.algorithm.terminationChromosomeCount);
-COUNT_CHOSENS = uint16(geneticAlgorithm.algorithm.chosensCount);
-COUNT_ELITES = uint16(geneticAlgorithm.algorithm.elitesCount);
+CROSSOVER_METHODS = dir('./crossoverMethods/x_*.m');
+COUNT_CHROMOSOMES = geneticAlgorithm.population.chromosomeCount;
+COUNT_GENES = uint16(geneticAlgorithm.population.geneCount);
+GENE_LOW_LIMIT = uint16(geneticAlgorithm.population.geneLowerLimit);
+GENE_UP_LIMIT = uint16(geneticAlgorithm.population.geneUpperLimit);
+TERMINATION_ERROR_PERCENTAGE = single(geneticAlgorithm.termination.terminationPercentage);
+TERMINATION_CHROMOSOME_COUNT = uint16(geneticAlgorithm.termination.terminationChromosomeCount);
+COUNT_CHOSENS = uint16(geneticAlgorithm.selection.chosensCount);
+COUNT_ELITES = uint16(geneticAlgorithm.selection.elitesCount);
 % Check for chosens and elites counts are valid
 assert(COUNT_CHOSENS <= COUNT_CHROMOSOMES, "geneticAlgorithm.json: <chosensCount> cannot exceed <chromosomeCount>");
 assert(COUNT_ELITES <= COUNT_CHROMOSOMES, "geneticAlgorithm.json: <elitesCount> cannot exceed <chromosomeCount>");
 COUNT_OFFSPRINGS = COUNT_CHROMOSOMES - COUNT_ELITES;
+MUTATION_CHROMOSOME_PERCENT = geneticAlgorithm.mutation.initialChromosomePercentage;
+MUTANT_CHROMOSOME_EFFECT = geneticAlgorithm.mutation.effectEachChromosome;
+MUTATION_GENE_PERCENT = geneticAlgorithm.mutation.initialGenePercentage;
+MUTANT_GENE_EFFECT = geneticAlgorithm.mutation.effectEachGene;
+MUTATION_RATE = geneticAlgorithm.mutation.mutationRate;
+
+%% 
+% Set global random number stream
+% RandStream.setGlobalStream(RandStream('dsfmt19937'));
