3f9f7742fc87e2a3b3ab5e3188a7c17766882ed7
Ferhat Yılmaz <dore.fy@gmail.com>
2021-01-14 13:14:16 +0300

    "generateUsageVector()": Edited.
    Global variable removed and inserted as parameter.
    
     Changes to be committed:
    	modified:   generateUsageVector.m
    
    
    Former-commit-id: 45dcb1d7800268ac22642fb0f3833f2192082f81

--- lib/dataGenerator/generateUsageVector.m
+++ lib/dataGenerator/generateUsageVector.m
@@ -10,15 +10,14 @@
 	Size of the vector determined by sample period.
 
 >> Inputs:
+	1. <COUNT_SAMPLE_IN_DAY> : integer : Sample count in a day
 
 << Outputs:
 	1. <usageVector> : matrix : Generated usage vector
 %}
 
 %%
-function usageVector = generateUsageVector()
-	global COUNT_SAMPLE_IN_DAY;
-	
+function usageVector = generateUsageVector(COUNT_SAMPLE_IN_DAY)
 	% Generate usage vector filled by zero
 	usageVector = single(zeros(1, COUNT_SAMPLE_IN_DAY));
 end
