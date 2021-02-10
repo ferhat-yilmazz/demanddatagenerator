89c2687e61b2b28c502d56f1de3299129d36320e
Ferhat Yılmaz <dore.fy@gmail.com>
2021-02-10 22:10:03 +0300

    "generateData.m": Edited.
    It completely changed according to new data structure and parfor
    integration.
    
    
    Former-commit-id: 46b1a2d632fd5d19feab4859eb291d893435bb99

--- lib/dataGenerator/generateData.m
+++ lib/dataGenerator/generateData.m
@@ -1,35 +1,45 @@
-% Add path of TRNG function
-addpath(strcat('.', filesep, 'lib', filesep, 'trueRandom', filesep));
-
 tic
+% Add path of TRNG function
+addpath(strcat('..', filesep, 'trueRandom', filesep));
 
 % Load configurations
 loadConfigurations;
 % Load global variables
-globalVariables;
+readOnlyVariables;
 
-% Generate structure for end-users
-endUsers = struct('type', [], 'properties', [], 'appliances', [], 'ev', []);
+% Build <baseStructure>
+baseStructure = buildBaseStructure(appliancesData, residentalTypes, electricVehicles, SAMPLE_PERIOD, GLOB_MAX_OPERATION_LIMIT);
+
+% Define <endUsers> structure
+endUsers = struct('typeID', uint8(0),...
+									'appliances', struct('applianceID', uint8(0),...
+																			 'applianceName', '',...
+																			 'duc', single(0),...
+																			 'wuc', single(0),...
+																			 'tuc', single(0),...
+																			 'usageArray', single([])),...
+									'EVs', struct('evID', uint8(0),...
+															 'evName', '',...
+															 'usageArray', single([])),...
+									'totalUsage', single([]));
 % Assign types to end-users
-endUsers = assignType2endUsers(residentalTypes, endUsers);
+endUsers = assignType2endUsers(endUsers, baseStructure, COUNT_END_USERS, RAND_METHOD);
 % Assign appliances to end-users
-endUsers = assignAppliances2endUsers(appliancesData, electricVehicles, endUsers);
+endUsers = assignAppliances2endUsers(endUsers, baseStructure, COUNT_SAMPLE_IN_DAY, COUNT_END_USERS, COUNT_DAYS, RAND_METHOD);
 % Check for work time constraints
-endUsers = check_workTimeConstraints(endUsers, appliancesData);
+endUsers = check_workTimeConstraints(endUsers, baseStructure, COUNT_SAMPLE_IN_DAY, COUNT_END_USERS, COUNT_WEEKS, COUNT_DAYS);
 % Assign worktimes and power values
-endUsers = assignWorktimes2appliances(endUsers, appliancesData, initialConditions);
-% Assign 0 to -1 filled samples
-endUsers = zero2minusOne(endUsers);
-% Build <mainUsageStructure>
-mainUsageStructure = buildMainUsageStructure(endUsers);
+endUsers = assignWorktimes2appliances(endUsers, baseStructure,runProbabilityParameters, defaultCoefficients, SAMPLE_PERIOD,...
+																			COUNT_SAMPLE_IN_DAY, COUNT_END_USERS, COUNT_WEEKS, DAY_PIECE);
+% Assign 0 to non-positive values in usage arrays
+endUsers = eliminateNegatives(endUsers, COUNT_END_USERS);
+% Totalize usage arrays for each end-user
+endUsers = totalizeUsageArrays(endUsers, baseStructure, COUNT_END_USERS, COUNT_WEEKS, COUNT_SAMPLE_IN_DAY);
 
+
+%% Clear Unnecessary Variables
+clear PATH_applianceData PATH_residentalTypes PATH_electricVehicles PATH_defaultCoefficients PATH_initialConditions...
+			PATH_runProbabilityParameters runProbabilityParametersFileNames runProbabilityParametersFiles...
+			SAMPLE_PERIOD RAND_METHOD msg GLOB_MAX_OPERATION_LIMIT file_idx DAY_PIECE COUNT_WEEKS...
+			COUNT_SAMPLE_IN_DAY COUNT_END_USERS COUNT_DAYS
 toc
-
-%% Clear Unnecessary variables
-clear PATH_applianceData PATH_residentalTypes PATH_electricVehicles...
-			PATH_defaultCoefficients PATH_initialConditions msg
-		
-%% Clear Global Variables
-clear global COUNT_END_USERS global COUNT_SAMPLE_IN_DAY global COUNT_WEEKS...
-	global SAMPLE_PERIOD global TRY_LIMIT global RAND_METHOD global DAY_PIECE...
-	global BATTERY_LEVEL_RAND_LIMIT global GLOB_MAX_OPERATION_LIMIT
